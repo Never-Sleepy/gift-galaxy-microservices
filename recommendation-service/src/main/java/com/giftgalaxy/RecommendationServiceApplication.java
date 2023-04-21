5ff80041a238fcc9adc819b2f5e9f1b5cad92ef3
TingSyuanWang <vincent@fishboneapps.com>
2023-04-20 22:38:27 -0400

    feat: generate the skeleton for microservices
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ recommendation-service/src/main/java/com/giftgalaxy/RecommendationServiceApplication.java
@@ -0,0 +1,26 @@
+package com.giftgalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1/ping")
+public class RecommendationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationServiceApplication.class, args);
+    }
+
+    @GetMapping()
+    public Map<String, String> ping() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "pong from recommendation service!");
+        return response;
+    }
+}
---
+++ user-service/src/main/java/com/giftgalaxy/UserServiceApplication.java
@@ -0,0 +1,24 @@
+package com.giftgalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1/ping")
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @GetMapping()
+    public Map<String, String> ping() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "pong from user service!");
+        return response;
+    }
+}
